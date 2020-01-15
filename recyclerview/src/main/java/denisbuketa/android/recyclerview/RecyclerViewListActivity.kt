ec30f5c8823b6fc27a600c2a54f95502938576b9
Denis Buketa <denis.buketa@fiveminutes.eu>
2020-01-15 10:29:18 +0100

    Setup dependencies for RecyclerView implementation

---
+++ recyclerview/src/main/java/denisbuketa/android/recyclerview/RecyclerViewListActivity.kt
@@ -0,0 +1,12 @@
+package denisbuketa.android.recyclerview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RecyclerViewListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler_view_list)
+    }
+}
