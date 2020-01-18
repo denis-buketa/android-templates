242b5088677b34ed2f940960e3d9cdf97a9979ca
Denis Buketa <denis.buketa@fiveminutes.eu>
2020-01-18 16:34:09 +0100

    Test

---
+++ recyclerview/build/generated/source/buildConfig/debug/denisbuketa/android/recyclerview/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package denisbuketa.android.recyclerview;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "denisbuketa.android.recyclerview";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "denisbuketa.android.recyclerview";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
